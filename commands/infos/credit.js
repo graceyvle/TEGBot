f21171acecf0d41b888f18a2dc76742dfcb0d976
Philipp-spec <subwayphil801@gmail.com>
2021-03-26 11:50:01 +0100

    Update credit.js

--- commands/infos/credit.js
+++ commands/infos/credit.js
@@ -16,6 +16,7 @@ module.exports = {
                     { name: 'moderation:', value: "devloped by: Patriot Zest "  },
                     {name: `welcome`, value: "developed by: Patriot Zest"},
                     {name: `suggestions`, value:  "devloped by:  Patriot Zest"},
+                    {name: `leveling`, value:  "devloped by:  Patriot Zest"},
                     { name: 'Music:', value: "devloped by: Philipp "  },
                     {name: `embed-messsages`, value: "devloped by: Philipp"},
                     {name: `organizer`, value: "Benthetechguy"}
