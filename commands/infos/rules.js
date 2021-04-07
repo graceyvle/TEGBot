38ccccb8b89d6c2489b213fb1bb3233f9edcb4c1
Philipp-spec <subwayphil801@gmail.com>
2021-04-07 20:42:14 +0200

    Update rules.js

--- commands/infos/rules.js
+++ commands/infos/rules.js
@@ -21,7 +21,8 @@ module.exports = {
                     {name: `#6` ,value: "No self promotion allowed. Failure to do so follows with the appropriate sanction."},
                     {name: `#7` ,value: "Do not leak or request any personal information (i.e. IP Addresses, Full Names, Dates of Birth etc."},
                     {name: `#8` ,value: " Please do not give any false or misleading information when helping someone with a case. If you want to help then please apply via our staff application forms."},
-                    {name: `#9` ,value: " English is the primary language of this server, should you require help in another language then please open a ticket in #ticket-create and we can advise you there."}
+                    {name: `#9` ,value: " English is the primary language of this server, should you require help in another language then please open a ticket in #ticket-create and we can advise you there."},
+                    {name: `#10`, value: "We do not offer help/support with pirated or cracked content. It is against our rules and it is illegal. This will result in the appropriate sanction."}
 
                     
                     
